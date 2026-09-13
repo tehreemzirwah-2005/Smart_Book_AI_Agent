50c950f122df7cdec1da6b13f54df00438b0d351
tehreemzirwah-2005 <tehreemzirwah@gmail.com>
2026-09-13 12:17:41 +0500

    Update app.py

--- app.py
+++ app.py
@@ -190,7 +190,7 @@ with tab4:
                         # Exact Verified Groq Active Model Setup
                         llm = ChatGroq(
                             groq_api_key=GROQ_API_KEY,
-                            model_name="llama-3.1-8b-instant",
+                            model_name="openai/gpt-oss-120b",
                             temperature=0.3
                         )
                         
